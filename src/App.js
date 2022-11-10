b1567bfdda144fb231b1e9f3a9cacd73ca83630e
Semiao91 <berna.1991.1@gmail.com>
2022-11-10 19:26:28 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [circle, setCircle] = useState([]);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const [globalCoords, setGlobalCoords] = useState({ x: 0, y: 0 });
+  const circleImg = <span className="dot" style={{ left: coords.x, top: coords.y }} />
+
+  useEffect(() => {
+    // get global mouse coordinates
+    console.log(circle)
+    const handleWindowMouseMove = event => {
+      setGlobalCoords({
+        x: event.screenX,
+        y: event.screenY,
+      });
+    };
+    window.addEventListener('mousemove', handleWindowMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleWindowMouseMove);
+    };
+  }, []);
+
+  const handleMouseMove = event => {
+    setCoords({
+      x: event.clientX - event.target.offsetLeft,
+      y: event.clientY - event.target.offsetTop,
+    });
+  };
+
+  function createCircle() {
+    setCircle(circle => [...circle, circleImg])
+  }
+
+  function eraseCircles() {
+    setCircle([])
+  }
+
+  function undoLastCircle() {
+    setCircle((circle) => (circle.slice(0, -1)));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <button onClick={undoLastCircle}>Undo</button>
+        <button onClick={eraseCircles}>Reset</button>
+      <div className='App-header' onMouseMove={handleMouseMove} onClick={createCircle}>
+        {circle}
+        <h2> Coords: x {coords.x} y {coords.y}</h2>
+      </div>
     </div>
   );
 }
